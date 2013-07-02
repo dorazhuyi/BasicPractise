2be867b7765df85ad99dbde9ba9d73d64db4c37b
nekozing <nekozing@gmail.com>
2013-07-02 19:21:23 +0900

    fixed mixed tabs and spaces

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -10,66 +10,66 @@ using namespace std;
 void merge(int* array, int p, int q, int r) 
 {
     // 1. each　new corresponds to a delete
-	int* left = new int[q - p + 2];
-	int* right = new int[r - q + 1];
-	int i, j, k;
-	for(i= 0; i<= q - p; i++) 
+    int* left = new int[q - p + 2];
+    int* right = new int[r - q + 1];
+    int i, j, k;
+    for(i= 0; i<= q - p; i++) 
     {
-		left[i] = array[p+i]; 
-	}
-	for(j= 0; j<= r - q - 1; j++) 
+        left[i] = array[p+i]; 
+    }
+    for(j= 0; j<= r - q - 1; j++) 
     {
-		right[j] = array[q+1+j]; 
-	}
+        right[j] = array[q+1+j]; 
+    }
  
-	left[i] = numeric_limits<int>::max();
-	right[j] = numeric_limits<int>::max();
+    left[i] = numeric_limits<int>::max();
+    right[j] = numeric_limits<int>::max();
  
-	i = j = 0;
-	for(k= p; k<= r; k++) 
+    i = j = 0;
+    for(k= p; k<= r; k++) 
     {
-		if(left[i] <= right[j])
-			array[k] = left[i++];
-		else 
-			array[k] = right[j++];
-	}
-	delete[] left;
-	delete[] right;
+        if(left[i] <= right[j])
+            array[k] = left[i++];
+        else 
+            array[k] = right[j++];
+    }
+    delete[] left;
+    delete[] right;
 }
  
 void mergesort(int* array, int p, int r)
 {
-	int q;
-	if(p < r)
-	{
-		q = floor((p+r) / 2);
-		mergesort(array, p, q);
-		mergesort(array, q+1, r);
-		merge(array, p, q, r);
-	}
+    int q;
+    if(p < r)
+    {
+        q = floor((p+r) / 2);
+        mergesort(array, p, q);
+        mergesort(array, q+1, r);
+        merge(array, p, q, r);
+    }
 }
 
 int main()
 {
     int* a = new int[10];
-	srand(time(nullptr));
-	for(int i= 0; i< 10; i++)
-	{
-		a[i] = rand() % 10;
-		// a[i] = 9 - i;
-		cout << a[i] ;
-	}
-	cout << endl;
- 
-	mergesort(a, 0, 9);
- 
-	cout << endl;
-	int i = 0;
-	while(i != 10)
+    srand(time(nullptr));
+    for(int i= 0; i< 10; i++)
     {
-		cout << a[i++];
-	}
-	cout << endl;
-	delete[] a;
-	cout << "finished .. woo hoo " << endl;
+        a[i] = rand() % 10;
+        // a[i] = 9 - i;
+        cout << a[i] ;
+    }
+    cout << endl;
+ 
+    mergesort(a, 0, 9);
+ 
+    cout << endl;
+    int i = 0;
+    while(i != 10)
+    {
+        cout << a[i++];
+    }
+    cout << endl;
+    delete[] a;
+    cout << "finished .. woo hoo " << endl;
 }

833802a8d39dc817390753081708b44c48004d58
Matcha <x8086x@gmail.com>
2013-07-03 20:39:33 +0900

    try playing with this one

--- InsertSort/IS.cpp
+++ InsertSort/IS.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
@@ -7,39 +8,42 @@ int main()
 {
     int n = 10;
     int* a = new int[n];
- 
+    srand(time(NULL)); // <- this gives the random number generator a seed 
+                       // try replacing time(NULL) with an int and run several times to see what happens
+
     int i, j;
     int key;
 
     /* Create Array */
     for(i = 0; i < 10; i++)
-    { 
-	a[i] = rand()%10;
-	cout << a[i];
+    {
+        a[i] = rand()%10;
+        cout << a[i];
     }
     cout << endl;
 
     /* Sort */
     for(i = 1; i < n; i++)
     {
-	key = a[i];
-	j = i - 1;
-	while( j >= 0 && key < a[j])
-	{
-	    a[j+1] = a[j];
-	    j--;
-	}
-	a[j+1] = key; // <- When I put "a[j] = key" here, it occured "double free/corruption" error. WHY???
+        key = a[i];
+        j = i - 1;
+        while( j >= 0 && key < a[j])
+        {
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = key; // <- When I put "a[j] = key" here, it occured "double free/corruption" error. WHY???
+                      // because the while loop ends with j = -1 , so ..
     }
 
     /* Display */
     for(i = 0; i < n; i++)
-    { 
-	cout << a[i];
+    {
+       cout << a[i];
     }
     cout << endl;
 
     delete[] a;
 
-    return 0; 
+    return 0;
 }
---
+++ InsertSort/InSort_01.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+// try figuring this one out
+int main()
+{
+    int n = 10;
+    int* a = new int[n];
+    srand(time(NULL));
+
+    /* Create Array */
+    for(int i = 0; i < 10; i++)
+    {
+        a[i] = rand()%10;
+        cout << a[i];
+    }
+    cout << endl;
+
+    /* Sort */
+    for(int i = 1; i < n; i++)
+    {
+        while(a[j] > a[j-1])
+        {
+            a[j] ^= a[j-1];
+            a[j-1] ^= a[j];
+            a[j--] ^= a[j-1];
+        }
+    }
+    /* Display */
+    for(i = 0; i < n; i++)
+    {
+       cout << a[i];
+    }
+    cout << endl;
+
+    delete[] a;
+
+    return 0;
+}
